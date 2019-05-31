4ae27e23e0efe8d26f22c653e98550658f398990
gary0130 <gary880130@gmail.com>
2019-05-31 10:47:44 +0800

    454

--- app/src/main/java/com/example/a0516/resultscreen.java
+++ app/src/main/java/com/example/a0516/resultscreen.java
@@ -1,6 +1,7 @@
 package com.example.a0516;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -27,7 +28,31 @@ public class resultscreen extends AppCompatActivity {
         double avg=sum/3.0;
         String text="Programming="+data1+"\nData Structure="+data2+"\nAlgorithm="+data3+"\nsum="+sum+"\naverage="+nf.format(avg);
         tvresult.setText(text);
+        ale(avg);
     }
     public void back(View v){finish();}
+
+    private void  ale(double avg){
+        String msg=new String();
+        String tit=new String();
+        int pic=0;
+        if(avg==100.0){
+            msg="太牛逼拉";
+            tit="PASS";
+            pic=R.drawable.pa;
+        }else if(avg>=60){
+            msg="正常發揮";
+            tit="PASS";
+            pic=R.drawable.pa;
+        }else{
+            msg="你個不阿";
+            tit="FAIL";
+            pic=R.drawable.fa;
+        }
+
+        AlertDialog.Builder builder =new AlertDialog.Builder(this);
+        builder.setTitle(tit).setMessage(msg).setIcon(pic);
+        builder.show();
+    }
 
 }
